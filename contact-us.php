4ef4d7c786c03db8cb926d2dfa480f8255cb44fb
harshwardhan sharma <hw.sharma9@gmail.com>
2022-02-13 17:26:57 +0530

    Contact us page updated

--- contact-us.php
+++ contact-us.php
@@ -87,11 +87,10 @@
                     <h2
                         class="text-gray-900 text-lg mb-1 font-medium title-font"
                     >
-                        Feedback
+                        Contact Us
                     </h2>
                     <p class="leading-relaxed mb-5 text-gray-600">
-                        Post-ironic portland shabby chic echo park, banjo
-                        fashion axe
+                        Book an appintment to meet us without any hurdle.
                     </p>
                     <div class="relative mb-4">
                         <label
@@ -103,6 +102,19 @@
                             type="email"
                             id="email"
                             name="email"
+                            class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                        />
+                    </div>
+                    <div class="relative mb-4">
+                        <label
+                            for="Mobile"
+                            class="leading-7 text-sm text-gray-600"
+                            >mobile</label
+                        >
+                        <input
+                            type="text"
+                            id="Mobile"
+                            name="mobile"
                             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                         />
                     </div>
