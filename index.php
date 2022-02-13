fead7fd76c5892cec2c9e9e0782de98a6b9396ce
harshwardhan sharma <hw.sharma9@gmail.com>
2022-02-13 17:20:44 +0530

    services section added in index.php file

--- index.php
+++ index.php
@@ -151,7 +151,7 @@
 
         <section class="text-gray-600 body-font">
             <div
-                class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
+                class="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center"
             >
                 <div
                     class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"
@@ -159,14 +159,15 @@
                     <h1
                         class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
                     >
-                        Before they sold out
-                        <br class="hidden lg:inline-block" />readymade gluten
+                        Tooth Bleaching
                     </h1>
                     <p class="mb-8 leading-relaxed">
-                        Copper mug try-hard pitchfork pour-over freegan heirloom
-                        neutra air plant cold-pressed tacos poke beard tote bag.
-                        Heirloom echo park mlkshk tote bag selvage hot chicken
-                        authentic tumeric truffaut hexagon try-hard chambray.
+                        Tooth whitening or tooth bleaching is the
+                        process of lightening the color of human teeth.
+                        Whitening is often desirable when teeth become
+                        yellowed over time for a number of reasons, and
+                        can be achieved by changing the intrinsic or
+                        extrinsic color of the tooth enamel.
                     </p>
                     <div class="flex justify-center">
                         <button
@@ -193,7 +194,7 @@
 
         <section class="text-gray-600 body-font">
             <div
-                class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center"
+                class="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center"
             >
                 <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
                     <img
@@ -208,14 +209,14 @@
                     <h1
                         class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"
                     >
-                        Before they sold out
-                        <br class="hidden lg:inline-block" />readymade gluten
+                        Dental implant
                     </h1>
                     <p class="mb-8 leading-relaxed">
-                        Copper mug try-hard pitchfork pour-over freegan heirloom
-                        neutra air plant cold-pressed tacos poke beard tote bag.
-                        Heirloom echo park mlkshk tote bag selvage hot chicken
-                        authentic tumeric truffaut hexagon try-hard chambray.
+                        A dental implant is a prosthesis that interfaces
+                        with the bone of the jaw or skull to support a
+                        dental prosthesis such as a crown, bridge,
+                        denture, or facial prosthesis or to act as an
+                        orthodontic anchor.
                     </p>
                     <div class="flex justify-center">
                         <button
@@ -260,6 +261,24 @@
                 }));
             });
         </script>
+
+        <section class="text-gray-600 body-font">
+            <div class="container mx-auto flex px-5 py-10 md:flex-row flex-col items-center">
+                <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">Before they sold out
+                    <br class="hidden lg:inline-block">readymade gluten
+                </h1>
+                <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                <div class="flex justify-center">
+                    <button class="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                    <button class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+                </div>
+                </div>
+                <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <img class="object-cover object-center rounded" alt="hero" src="./images/services.jpeg">
+                </div>
+            </div>
+        </section>
 
         <footer class="text-gray-600 body-font">
             <div
